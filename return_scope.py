ef6ef93afeba241b426be07811e9546e50aa0abb
Elyashiv <elyashiv988@gmail.com>
2026-06-29 15:12:26 +0300

    added part 1

--- return_scope.py
+++ return_scope.py
@@ -7,7 +7,7 @@
 # 7 both printed 20 because the command 'global' overright the global value point
 # 8 the function runs form inside to outside and the value status run only in the score,so firest def inner printed running, secend def outer printed ready, therd the global value status printed waiting. 
 # 9 def inner takes and over right the coins from def outer (bt the commend 'non local'). so def inner and def outer printed 16, but the global coins doesn't chande so it prints 10. 
-# 10 
+# 10 firet of all, all bag printed ['key', 'map', 'coin'] because when we add values to bag it changes the global bag because it's list so the varible just points by refus. the varible score in def inner takes and over right from def outer (by 'non local' commend) so in def outer score=10 , score=score*2 (total 20) then we call the fef inner: scroe=score+5 so in the inner score=25. then we call the outer who over right by inner so score = 25 also. but the global score hasn't changed so score=1. 
 
 score = 1
 bag = ["key"]
