9e35f5c8bd39c4056d0b997e1e7189c6e8c8e331
Elyashiv <elyashiv988@gmail.com>
2026-06-29 11:30:22 +0300

    added 5

--- return_scope.py
+++ return_scope.py
@@ -2,15 +2,14 @@
 # 2 the function printed Spy (name), 40 (level), and out the function printed agent(name), 2(level). 
 # 3 the function printed 30 ((5+10)*2), the volue coins printed 20. 
 # 4 the function printed 70 (100-30), 35(30+5). out of function the value health printed 100. 
-# 5 
+# 5 both printed ['map', 'key', 'torch', 'coin'] because when the function called <items> (list) it isn't copy the varible but just points the index of items in the memory so when we change items the memory has changed. 
+# 6 
 items = ["map", "key"]
 
-def add_item():
-    items.append("torch")
-    items.append("coin")
+def replace_items():
+    items = ["potion"]
+    items.append("shield")
     print(items)
 
-add_item()
+replace_items()
 print(items)
-
-
