56cd46bf532f26da817d211554220e96e6a7a78c
Elyashiv <elyashiv988@gmail.com>
2026-06-29 11:27:18 +0300

    added 5

---
+++ return_scope.py
@@ -0,0 +1,16 @@
+# 1 the function printed 8 and the value score out of the function printed 10. 
+# 2 the function printed Spy (name), 40 (level), and out the function printed agent(name), 2(level). 
+# 3 the function printed 30 ((5+10)*2), the volue coins printed 20. 
+# 4 the function printed 70 (100-30), 35(30+5). out of function the value health printed 100. 
+# 5 
+items = ["map", "key"]
+
+def add_item():
+    items.append("torch")
+    items.append("coin")
+    print(items)
+
+add_item()
+print(items)
+
+
